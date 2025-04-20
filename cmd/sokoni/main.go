d631865a500bb2e9f35250bbb12be071cad28934
koplec <koplec@github.com>
2025-04-20 20:57:36 +0900

    scanして一覧を表示するプログラム作成

--- cmd/sokoni/main.go
+++ cmd/sokoni/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/koplec/sokoni/internal/collector"
+)
 
 func main() {
 	fmt.Println("Hello sokoni!")
+
+	path := "/mnt/share"
+
+	files, err := collector.Scan(path)
+	if err != nil {
+		log.Fatalf("failed to scan files: %v", err)
+	}
+
+	fmt.Printf("Found %d PDF files:\n", len(files))
+	for _, f := range files {
+		fmt.Printf("- %s (%d bytes)=\n", f.Path, f.Size)
+	}
 }
