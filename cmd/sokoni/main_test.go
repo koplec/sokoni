bd6b263f93ad611950dd356b1bc42676b79a1bcc
koplec <koplec@github.com>
2025-04-20 15:59:07 +0900

    Hello sokoni

---
+++ cmd/sokoni/main_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestBasicGreeting(t *testing.T) {
+	t.Log("Basic greerting workds (output tested manually)")
+	t.Log("sokoniは元気です。Helloも出ました。")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/koplec/sokoni
+
+go 1.23.4
